1d4f1bdf4f9093bbbfac47ed731a28ae8df6c661
Makson047 <bogatyuk1998@gmail.com>
2022-04-27 17:27:00 +0300

    added checking an element on a page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
+# from selenium.common.exceptions import имя_исключения
 
 
 class BasePage(object):
-    def __init__(self, browser: webdriver, url):
+    def __init__(self, browser: webdriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except Exception as error:
+            print(error)
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
-
